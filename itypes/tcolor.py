f71bcc65c6abd5d591dafa0e160538fbc9d6a5f5
Ilnur Saifutdinov <ilnursoft@gmail.com>
2025-04-24 14:51:02 +0300

    add конструктор класса tcolor

--- itypes/tcolor.py
+++ itypes/tcolor.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel, Field
 class TColor(BaseModel):
     text: int = Field(..., ge=0, le=255)
     bg: int = Field(..., ge=0, le=255) 
+
+    def __init__(self, text: int, bg: int):
+        super().__init__(text=text, bg=bg)
+
+    def __str__(self):
+        return f"TColor(text={self.text}, bg={self.bg})"
+
+
