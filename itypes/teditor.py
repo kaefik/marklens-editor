9f170ae8621ad482b5f68e4e7d58cccbc81a9d10
Ilnur Saifutdinov <ilnursoft@gmail.com>
2025-04-25 17:36:47 +0300

    refactor TEditor: улучшена обработка размеров окна и добавлена обработка клавиш управления курсором

--- itypes/teditor.py
+++ itypes/teditor.py
@@ -4,6 +4,7 @@ from .tcolor import TColor
 from .tbuffer import TBuffer
 import curses
 from curses import wrapper
+import os
 
 
 class TEditor(BaseModel):
@@ -17,13 +18,23 @@ class TEditor(BaseModel):
 
     model_config = ConfigDict(arbitrary_types_allowed=True)  # Разрешаем любые типы
 
-    def __init__(self, cols: int, rows: int, color: TColor, buffer: TBuffer, row: int = 0, col: int =0):
+    def __init__(self,  rows: int, cols: int, color: TColor, buffer: TBuffer, row: int = 0, col: int =0):
         super().__init__(cols=cols, rows=rows, color=color, buffer=buffer, row=row, col=col)
         self.row = row
         self.col = col
-        self.screen = curses.initscr()
-        self.rows, self.cols = self.screen.getmaxyx()
 
+    def _main(self, stdscr):
+        # Получаем размеры терминала
+        max_y, max_x = stdscr.getmaxyx()
+        # Проверяем и корректируем размеры окна
+        self.rows = min(self.rows, max_y)
+        self.cols = min(self.cols, max_x)
+        
+        self.screen = curses.newwin(self.rows, self.cols, 0, 0)
+        #self.screen.keypad(True)
+        self.screen.clear()
+        
+        self.run()
 
     def init_colors(self, id: int = 1):
         curses.start_color()
@@ -39,60 +50,55 @@ class TEditor(BaseModel):
 
     def check_cursor(self):
         if self.col >= self.cols:
-                self.col = 0
-                self.row += 1
+            self.col = self.cols - 1
             
         if self.col < 0:
             self.col = 0
-            self.row -= 1
         
         if self.row >= self.rows:
             self.row = self.rows - 1
 
         if self.row < 0:
-            self.row = 0   
+            self.row = 0
 
     def run(self):
-
-        self.init_colors()
-        self.screen.keypad(True)  # Включаем обработку специальных клавиш
-    
-        while True:
-            # self.screen.addstr(0, 0, self.buffer.buffer)
-            # self.screen.addstr(0, self.buffer.cursor, self.buffer.buffer[self.buffer.cursor], curses.color_pair(1))
-            
-          
-            key = self.screen.getch()
-            print(f"{key=}")
-            if key == curses.KEY_UP:
-                self.row -= 1
-            elif key == curses.KEY_DOWN:
-                self.row += 1
-            elif key == curses.KEY_LEFT:
-                self.col -= 1
-            elif key == curses.KEY_RIGHT:
-                self.col += 1
-            elif key == 8:
-                self.col -= 1                 
-                self.check_cursor()
-                self.screen.addch(self.row, self.col, ' ')
-                
-            elif key in [10, 13, curses.KEY_ENTER]:  # Enter может быть 10, 13 или KEY_ENTER
-                self.row += 1
-                self.col = 0        
-            elif key == 17: # Ctrl+Q
-                break
-            else:
-                self.screen.addch(self.row, self.col, chr(key))
-                self.col += 1
-
-            self.check_cursor()
-            
-
-            self.screen.move(self.row, self.col)  # Устанавливаем курсор в текущую позицию
-            self.screen.refresh()
+        try:
+            self.init_colors()
+            self.screen.keypad(True)  # Включаем обработку специальных клавиш
         
-        #  curses.endwin()
+            while True:
+                key = self.screen.getch()
+                print(f"{key=}")
+                if key == curses.KEY_UP:
+                    self.row -= 1
+                elif key == curses.KEY_DOWN:
+                    self.row += 1
+                elif key == curses.KEY_LEFT:
+                    self.col -= 1
+                elif key == curses.KEY_RIGHT:
+                    self.col += 1
+                elif key == 8:
+                    self.col -= 1                 
+                    self.check_cursor()
+                    self.screen.addch(self.row, self.col, ' ')
+                    
+                elif key in [10, 13, curses.KEY_ENTER]:  # Enter может быть 10, 13 или KEY_ENTER
+                    self.row += 1
+                    self.col = 0        
+                elif key == 17: # Ctrl+Q
+                    break
+                else:
+                    self.screen.addch(self.row, self.col, chr(key))
+                    self.col += 1
+
+                self.check_cursor()
+                self.screen.move(self.row, self.col)
+                self.screen.refresh()
+        finally:
+            curses.endwin()
+
+    def start(self):
+        wrapper(self._main)
 
             
 
