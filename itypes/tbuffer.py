d0ce217967c0a10001a83d2a4f9c37cd2320f608
ilnursoft <ilnursoft@gmail.com>
2025-05-04 00:06:42 +0300

    del лишние сущности внутри буфера

--- itypes/tbuffer.py
+++ itypes/tbuffer.py
@@ -3,19 +3,22 @@ from typing import Optional, Tuple, List
 
 class TBuffer(BaseModel):
     buffer: List[str]
-    cursor_x: int = Field(..., ge=0)
-    cursor_y: int = Field(..., ge=0)
-    selection: Optional[Tuple[int, int]] = None
+    #cursor_x: int = Field(..., ge=0)
+    #cursor_y: int = Field(..., ge=0)
+    #selection: Optional[Tuple[int, int]] = None
 
-    def __init__(self, buffer: List[str], cursor_x: int =0, cursor_y: int=0, selection: Optional[Tuple[int, int]] = None):
-        super().__init__(buffer=buffer, cursor_x=cursor_x, cursor_y=cursor_y, selection=selection)
+    def __init__(self, buffer: List[str]):  #, cursor_x: int =0, cursor_y: int=0, selection: Optional[Tuple[int, int]] = None):
+        """
+        buffer - внутреннее представление содержимого буфера с текстом
+        """
+        super().__init__(buffer=buffer) #, cursor_x=cursor_x, cursor_y=cursor_y, selection=selection)
 
     
     def __str__(self):
         return "\n".join(self.buffer)
     
     def __repr__(self):
-        return f"TBuffer(buffer={self.buffer}, cursor_x={self.cursor_x}, cursor_y={self.cursor_y}, selection={self.selection})"
+        return f"TBuffer(buffer={self.buffer}"  #", cursor_x={self.cursor_x}, cursor_y={self.cursor_y}, selection={self.selection})"
     
     def __len__(self):
         return len(self.buffer)
